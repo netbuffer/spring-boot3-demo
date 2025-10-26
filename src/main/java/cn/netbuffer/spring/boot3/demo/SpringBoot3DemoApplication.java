117dac23b10b808f1388367f394a10b03f695b4e
netbuffer <javawiki@163.com>
2025-10-26 20:25:39 +0800

    init

---
+++ src/main/java/cn/netbuffer/spring/boot3/demo/SpringBoot3DemoApplication.java
@@ -0,0 +1,13 @@
+package cn.netbuffer.spring.boot3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/netbuffer/spring/boot3/demo/SpringBoot3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.netbuffer.spring.boot3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
